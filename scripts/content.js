e738af16b319b4945dc46fed53cab48003d31c68
Daniel Harris <danzo2303@gmail.com>
2024-12-13 23:24:52 +0000

    Puzzle3

--- scripts/content.js
+++ scripts/content.js
@@ -10,4 +10,10 @@ const PUZZLES = [
     ["toad","wall","fire","sesame"],
     [[3,4], [2,4], [1,3],[2]]
   ),
+
+  new Puzzle(
+    ["gymnast",'instrument','furniture','glass'],
+    ["flute","piano","chest","tumbler"],
+    [[4], [1,2], [2,3],[1,4]]
+  ),
 ];
