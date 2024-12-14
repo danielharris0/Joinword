8f539da6c28ff04378a3f219dbdcbc4db94e8a35
Daniel Harris <danzo2303@gmail.com>
2024-12-14 00:05:59 +0000

    Puzzle4

--- scripts/content.js
+++ scripts/content.js
@@ -16,4 +16,10 @@ const PUZZLES = [
     ["flute","piano","chest","tumbler"],
     [[4], [1,2], [2,3],[1,4]]
   ),
+
+  new Puzzle(
+    ["alphabetic homophone",'might begin a question','wherefore','form of the verb \'is\''],
+    ["see","be","are","why"],
+    [[1,2,3,4],[3,4],[4],[2,3]]
+  ),
 ];
